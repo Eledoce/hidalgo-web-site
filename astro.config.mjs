2cb348d0a519c38a7a474d7c8219e3aae359c869
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-06 20:05:36 -0600

    Update URLs and imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://eledoce.github.io/hidalgo-web-site/',
-  base: 'https://eledoce.github.io//hidalgo-web-site/',
+  site: 'https://eledoce.github.io/',
+  base: '/hidalgo-web-site/',
 })
---
+++ src/scripts/url.js
@@ -0,0 +1,1 @@
+export const url = '/hidalgo-web-site'
